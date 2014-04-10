5502d03dfddbdbb24a319c4414657ea9ceaf65d6
Matt Ricketson <mricketson@mac.com>
2014-04-10 15:50:29 -0400

    Updates PhyreKit.h

--- Classes/PhyreKit.h
+++ Classes/PhyreKit.h
@@ -9,7 +9,22 @@
 #ifndef _PhyreKit_h
 #define _PhyreKit_h
 
-#import "NSObject+PhyreKit.h"
-#import "NSString+PhyreKit.h"
+#import <PhyreKit/PHYConstantsAndMacros.h>
+
+#import <PhyreKit/NSObject+PhyreKit.h>
+#import <PhyreKit/NSString+PhyreKit.h>
+#import <PhyreKit/NSAttributedString+PhyreKit.h>
+#import <PhyreKit/NSArray+PhyreKit.h>
+#import <PhyreKit/NSDictionary+PhyreKit.h>
+#import <PhyreKit/NSCache+PhyreKit.h>
+
+#import <PhyreKit/UIFont+PhyreKit.h>
+#import <PhyreKit/UIImage+PhyreKit.h>
+#import <PhyreKit/UIViewController+PhyreKit.h>
+
+#import <PhyreKit/CLLocation+PhyreKit.h>
+
+#import <PhyreKit/UIColor+Expanded.h>
+#import <PhyreKit/UIImage+ImageEffects.h>
 
 #endif
